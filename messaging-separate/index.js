5e19ed52caa0db30546892386514ab634e37dbcf
Tim <tim.channell@gmail.com>
2015-11-11 14:53:56 +0000

    pulled in messaging separate

---
+++ messaging-separate/index.js
@@ -0,0 +1,31 @@
+
+var fs = require('fs'),
+  fork = require('child_process').fork,
+  path = require('path'),
+  cpus = require('os').cpus().length;
+
+var donecount = 0;
+var sendcount = 20;
+var workers = [];
+
+console.error("creating "+cpus+" workers")
+for (var i = 0; i < cpus; i++) {
+  var worker = fork(path.join(__dirname, 'worker.js'), [i]);
+  workers.push(worker);
+  worker.on('message', function (dt) {
+    if (dt === "end") { 
+      console.error(++donecount, sendcount);
+      if (donecount == sendcount) {
+        process.exit();
+      }
+    } else {
+	process.stdout.write(JSON.stringify(dt)+'\n');
+    }
+  });
+}
+
+
+for (var i = 0; i < sendcount; i++) {
+  workers[i % workers.length].send(i);
+}
+
